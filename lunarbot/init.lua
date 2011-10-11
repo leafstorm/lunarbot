c45ea0ff425bc3acede2339e21e0639fc4560cd0
Matthew Frazier <leafstormrush@gmail.com>
2011-10-11 12:06:20 -0400

    turned lunarbot into a package
    Most of the code remains in lunarbot.init, but some of the more general
    stuff was moved to lunarbot.utils.

--- lunarbot/init.lua
+++ lunarbot/init.lua
@@ -2,70 +2,9 @@
 -- Lunarbot is a bot written in Lua. It is powered by LuaIRC.
 -- Like most bots, it uses plugins.
 
+require 'lunarbot.utils'
 local ircmodule = require 'irc'
 local sleep = require 'socket'.sleep
-
-function printf (mesg, ...)
-    print(string.format(mesg, ...))
-end
-
-
-function new (cls, ...)
-    local instance = setmetatable({}, cls)
-    if cls.__init then
-        instance:__init(...)
-    end
-    return instance
-end
-
-
-function class ()
-    local cls = {}
-    cls.__index = cls
-    cls.new = new
-    return cls
-end
-
-
-Scheduler = class()
-
-function Scheduler:__init ()
-    self.events = {}
-end
-
-
-function Scheduler:check ()
-    local idx = 1
-    local events = self.events
-    while idx <= #events do
-        local event = events[idx]
-        local keep = event()
-        if keep then
-            idx = idx + 1
-        else
-            table.remove(events, idx)
-        end
-    end
-end
-
-
-function Scheduler:add (event)
-    self.events[#self.events + 1] = event
-end
-
-
-function Scheduler:after (delay, event)
-    local firetime = os.time() + delay
-    self:add(function ()
-        if os.time() > firetime then
-            event()
-            return false
-        else
-            return true
-        end
-    end)
-end
-
 
 function plugin (name)
     return {name = name, callbacks = {}}
---
+++ lunarbot/utils.lua
@@ -0,0 +1,63 @@
+--- Utilities used by Lunarbot.
+-- Yes, I know these are all globals. I'll fix it eventually.
+
+function printf (mesg, ...)
+    print(string.format(mesg, ...))
+end
+
+
+function new (cls, ...)
+    local instance = setmetatable({}, cls)
+    if cls.__init then
+        instance:__init(...)
+    end
+    return instance
+end
+
+
+function class ()
+    local cls = {}
+    cls.__index = cls
+    cls.new = new
+    return cls
+end
+
+
+Scheduler = class()
+
+function Scheduler:__init ()
+    self.events = {}
+end
+
+
+function Scheduler:check ()
+    local idx = 1
+    local events = self.events
+    while idx <= #events do
+        local event = events[idx]
+        local keep = event()
+        if keep then
+            idx = idx + 1
+        else
+            table.remove(events, idx)
+        end
+    end
+end
+
+
+function Scheduler:add (event)
+    self.events[#self.events + 1] = event
+end
+
+
+function Scheduler:after (delay, event)
+    local firetime = os.time() + delay
+    self:add(function ()
+        if os.time() > firetime then
+            event()
+            return false
+        else
+            return true
+        end
+    end)
+end
